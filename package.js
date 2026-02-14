d365e7f618c29f9fefc8d1343aa9ee7ee9bf3f67
bbbbn1217333-beep <bbbbn1217333@gmail.com>
2026-02-14 14:32:50 +0900

    Create package.js

---
+++ package.js
@@ -0,0 +1,10 @@
+{
+  "name": "ira-solrank-api",
+  "version": "1.0.0",
+  "description": "이라네 솔랭 API",
+  "main": "api/sync.js",
+  "dependencies": {
+    "@supabase/supabase-js": "^2.38.4",
+    "node-fetch": "^2.7.0"
+  }
+}
